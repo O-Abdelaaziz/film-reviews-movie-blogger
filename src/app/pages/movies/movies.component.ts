a585728f552d85c88ed30b93048f22b8ae29d7dd
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-01-03 10:20:51 +0100

    Task 9: create home & movies components

---
+++ src/app/pages/movies/movies.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styles: [
+  ]
+})
+export class MoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
