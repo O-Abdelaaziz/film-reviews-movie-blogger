245259dcc0b3140296a0cc2274fb5ec718194e15
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-01-04 09:00:06 +0100

    Task 12: update movie service (getting popular movies with api key)

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  private THE_MOVIE_DB_API_KEY="";
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  getMovies() {
+    return this._httpClient.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${this.THE_MOVIE_DB_API_KEY}&language=en-US&page=1`);
+  }
 }
