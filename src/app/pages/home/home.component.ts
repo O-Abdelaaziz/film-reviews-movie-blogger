6ca0be2420621cc5a563a9b00df7559184a07e0e
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-01-05 08:22:35 +0100

    Task 15: create log engine service & update home component (centralize logging using service)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MoviesService} from "../../services/movies.service";
+import {LogEngineService} from "../../shared/services/logs/log-engine.service";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,9 @@ import {MoviesService} from "../../services/movies.service";
 export class HomeComponent implements OnInit {
   movies: any = [];
 
-  constructor(private _moviesService: MoviesService) {
+  constructor(
+    private _moviesService: MoviesService,
+    private _logEngineService: LogEngineService) {
   }
 
   ngOnInit(): void {
@@ -20,7 +23,9 @@ export class HomeComponent implements OnInit {
     this._moviesService.getMovies().subscribe(
       (response: any) => {
         this.movies = response.results;
-        console.log(this.movies);
+        //output
+        //[HomeComponent] [Info] [onGetMoviesList] [05/01/2022, 09:19:37] [Movies Data]
+        this._logEngineService.info(JSON.stringify(this.movies), this.constructor.name, this.onGetMoviesList.name);
       }
     )
   }
