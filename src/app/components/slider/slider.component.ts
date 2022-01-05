2fd766130a3e0425f7005389c10e127a32f30867
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-01-05 10:30:56 +0100

    Task 21: use component inputs to pass the movie data (input decorator)

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public items: any;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
