d83a3c219c90343b4ec452197afeb8bc578d6a2a
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-01-05 15:00:26 +0100

    Task 30: create movie type enum & update movies service (get movies depands on their type)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MoviesService} from "../../services/movies.service";
 import {LogEngineService} from "../../shared/services/logs/log-engine.service";
+import {Movie} from "../../models/movie";
+import {MovieType} from "../../shared/enums/movie-type";
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,11 @@ import {LogEngineService} from "../../shared/services/logs/log-engine.service";
   styles: []
 })
 export class HomeComponent implements OnInit {
-  movies: any = [];
+  public latestMovies: Movie[] = [];
+  public upcomingMovies: Movie[] = [];
+  public topRatedMovies: Movie[] = [];
+  public popularMovies: Movie[] = [];
+  public nowPlayingMovies: Movie[] = [];
 
   constructor(
     private _moviesService: MoviesService,
@@ -16,17 +22,63 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.onGetMoviesList();
+    this.onGetLatestMovies();
   }
 
-  onGetMoviesList() {
-    this._moviesService.getMovies().subscribe(
+  //region Populate movie list depends on their type
+  onGetLatestMovies() {
+    this._moviesService.getMovies(MovieType.LATEST).subscribe(
       (response: any) => {
-        this.movies = response.results;
+        this.latestMovies = response.results;
         //output
         //[HomeComponent] [Info] [onGetMoviesList] [05/01/2022, 09:19:37] [Movies Data]
-        this._logEngineService.info(JSON.stringify(this.movies), this.constructor.name, this.onGetMoviesList.name);
+        this._logEngineService.info(JSON.stringify(this.latestMovies), this.constructor.name, this.onGetLatestMovies.name);
       }
     )
   }
+
+  onGetUpcomingMovies() {
+    this._moviesService.getMovies(MovieType.UPCOMING).subscribe(
+      (response: any) => {
+        this.upcomingMovies = response.results;
+        //output
+        //[HomeComponent] [Info] [onGetMoviesList] [05/01/2022, 09:19:37] [Movies Data]
+        this._logEngineService.info(JSON.stringify(this.upcomingMovies), this.constructor.name, this.onGetUpcomingMovies.name);
+      }
+    )
+  }
+
+  onGetTopRatedMovies() {
+    this._moviesService.getMovies(MovieType.TOP_RATED).subscribe(
+      (response: any) => {
+        this.topRatedMovies = response.results;
+        //output
+        //[HomeComponent] [Info] [onGetMoviesList] [05/01/2022, 09:19:37] [Movies Data]
+        this._logEngineService.info(JSON.stringify(this.topRatedMovies), this.constructor.name, this.onGetTopRatedMovies.name);
+      }
+    )
+  }
+
+  onGetPopularMovies() {
+    this._moviesService.getMovies(MovieType.POPULAR).subscribe(
+      (response: any) => {
+        this.popularMovies = response.results;
+        //output
+        //[HomeComponent] [Info] [onGetMoviesList] [05/01/2022, 09:19:37] [Movies Data]
+        this._logEngineService.info(JSON.stringify(this.popularMovies), this.constructor.name, this.onGetPopularMovies.name);
+      }
+    )
+  }
+
+  onGetNowPlayingMovies() {
+    this._moviesService.getMovies(MovieType.NOW_PLAYING).subscribe(
+      (response: any) => {
+        this.nowPlayingMovies = response.results;
+        //output
+        //[HomeComponent] [Info] [onGetMoviesList] [05/01/2022, 09:19:37] [Movies Data]
+        this._logEngineService.info(JSON.stringify(this.nowPlayingMovies), this.constructor.name, this.onGetNowPlayingMovies.name);
+      }
+    )
+  }
+  //endregion
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,18 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {MovieType} from "../shared/enums/movie-type";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
   private THE_MOVIE_DB_API_KEY = environment.env.API_KEY;
+  private THE_MOVIE_DB_BASE_URL = environment.MOVIE_DB_BASE_URL;
 
   constructor(private _httpClient: HttpClient) {
   }
 
-  getMovies() {
+  getMovies(movieType: MovieType = MovieType.UPCOMING) {
     console.log(this.THE_MOVIE_DB_API_KEY);
-    return this._httpClient.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${this.THE_MOVIE_DB_API_KEY}&language=en-US&page=1`);
+    return this._httpClient.get(`${this.THE_MOVIE_DB_BASE_URL}/movie/${movieType}?api_key=${this.THE_MOVIE_DB_API_KEY}&language=en-US&page=1`);
   }
 }
